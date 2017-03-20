215821c15fce0a059fdb880c90e6674f15203b44
Steven Simpson <s.simpson@lancaster.ac.uk>
2017-03-20 15:26:55 +0000

    Hooked OpenSSL implementation of SHA-256 into agent.

--- includes/ebpf_digest.h
+++ includes/ebpf_digest.h
@@ -3,6 +3,8 @@
 #ifndef __EBPF_SWITCH_DIGEST_H
 #define __EBPF_SWITCH_DIGEST_H
 
+#include <openssl/sha.h>
+
 enum digest {
     digest_SHA256,
 };
@@ -11,13 +13,20 @@ enum digest {
 #define DIGEST_SIZE(DT) DIGEST_SIZE_ ## DT
 #define DIGEST(DT) digest_ ## DT ## _t
 
+#define DECLARE_DIGEST_TYPE(DT) \
+    typedef unsigned char DIGEST(DT)[DIGEST_SIZE(DT)]
+
 struct digest_ctxt {
     enum digest type;
 };
 
+#define DIGEST_SIZE_SHA256 SHA256_DIGEST_LENGTH
+
 struct digest_ctxt_SHA256 {
     enum digest type;
-    /* TODO */
+    SHA256_CTX state;
 };
+
+DECLARE_DIGEST_TYPE(SHA256);
 
 #endif
