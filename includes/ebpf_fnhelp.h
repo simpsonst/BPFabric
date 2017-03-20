b12a322838724a70a77a23a8746fd6dbd9eb3463
Steven Simpson <s.simpson@lancaster.ac.uk>
2017-03-20 11:56:19 +0000

    Added macros to check exposed function signatures in userspace, and register them.

--- includes/ebpf_fnhelp.h
+++ includes/ebpf_fnhelp.h
@@ -11,4 +11,14 @@
 #define BPF_FNDEFNAS(N, A) BPF_FNAS(N, A) = (BPF_FNTYPE(N) *) BPF_FUNC_ ## N
 #define BPF_FNDEFN(N) BPF_FNDEFNAS(N, N)
 
+#if ! __BPF__
+/* Use these from native code to check signatures. */
+#define BPF_FNCHECKAS(N, A) extern BPF_FNTYPE(N) A
+#define BPF_FNCHECK(N) BPF_FNCHECKAS(N, N)
+
+/* Expose a function to the VM. */
+#define BPF_REGISTERAS(VM, N, A) ubpf_register((VM), BPF_FUNC_ ## N, #N, (A))
+#define BPF_REGISTER(VM, N) BPF_REGISTERAS((VM), N, N)
+#endif
+
 #endif
