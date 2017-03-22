12a2a14a8f2ba859bbb5550e9858d630a4cf155f
Steven Simpson <s.simpson@lancaster.ac.uk>
2017-03-22 14:15:01 +0000

    BPF-external functions accessible when BPF code compiled as native.

--- includes/ebpf_fnhelp.h
+++ includes/ebpf_fnhelp.h
@@ -4,14 +4,17 @@
 #define __EBPF_SWITCH_FNHELP_H
 
 #define BPF_FNTYPE(N) bpf_ ## N ## _ft
-
-#define BPF_FNAS(N, A) static BPF_FNTYPE(N) *const A
 #define BPF_FN(N) BPF_FNAS(N, N)
-
-#define BPF_FNDEFNAS(N, A) BPF_FNAS(N, A) = (BPF_FNTYPE(N) *) BPF_FUNC_ ## N
 #define BPF_FNDEFN(N) BPF_FNDEFNAS(N, N)
 
-#if ! __BPF__
+#if  __BPF__
+#define BPF_FNAS(N, A) static BPF_FNTYPE(N) *const A
+#define BPF_FNDEFNAS(N, A) BPF_FNAS(N, A) = (BPF_FNTYPE(N) *) BPF_FUNC_ ## N
+
+#else
+#define BPF_FNAS(N, A) BPF_FNTYPE(N) A
+#define BPF_FNDEFNAS(N, A) BPF_FNAS(N, A)
+
 /* Use these from native code to check signatures. */
 #define BPF_FNCHECKAS(N, A) BPF_FNTYPE(N) A
 #define BPF_FNCHECK(N) BPF_FNCHECKAS(N, N)
