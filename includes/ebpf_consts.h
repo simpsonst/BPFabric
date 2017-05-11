b21b6c19531260cf7c24c27f2021b004cdb4ab92
Steven Simpson <s.simpson@lancaster.ac.uk>
2017-05-11 15:47:55 +0100

    Compatibility with newer clang.

--- includes/ebpf_consts.h
+++ includes/ebpf_consts.h
@@ -8,6 +8,13 @@
 #define CONTROLLER 0xfffffffe
 #define DROP       0xffffffff
 
+#if __BPF__ && __clang_major__ >= 5
+#define BPF_STATIC_FUNC static inline
+#define inline __attribute__((always_inline))
+#else
+#define BPF_STATIC_FUNC static
+#endif
+
 struct metadatahdr { // limited to the size available between the TPACKET_V2 header and the tp_mac payload
     uint32_t in_port;
     uint32_t sec;
